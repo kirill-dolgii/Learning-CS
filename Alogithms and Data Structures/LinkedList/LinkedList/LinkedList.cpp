8654a2165aeb85663b26ff68372e23e257022989
Kirill Dolgiy <maindolgiykirill99@gmail.com>
2022-01-02 12:46:22 +0300

    Node class implemented.

--- Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
+++ Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
 
+struct Node
+{
+public:
+    Node* Next;
+    int Data;
+
+    Node(Node* next, int data) {
+        this->Next = next;
+        this->Data = data;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
