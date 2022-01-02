da80b9dea0ee0afc7b93bb1afb687bee1f305444
Kirill Dolgiy <maindolgiykirill99@gmail.com>
2022-01-02 12:47:07 +0300

    List class with isEmpty and Add methods is implemented.

--- Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
+++ Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
@@ -12,6 +12,39 @@ public:
     }
 };
 
+class List
+{
+public:
+    Node* First;
+
+    List(Node* node) {
+        this->First = node;
+    }
+
+    List() {
+        this->First = nullptr;
+    }
+
+    bool IsEmpty() {
+        return this->First == NULL;
+    }
+
+    void Add(Node* node) {
+        if (this->IsEmpty() != true) {
+            Node* tmp = this->First;
+
+            this->First = node;
+            this->First->Next = tmp;
+
+            free(tmp);
+        }
+        else {
+            this->First = node;
+        }
+
+    }
+};
+
 int main()
 {
 
