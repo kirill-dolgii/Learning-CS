40126f88c6c2acdcfc4a2acf35a213f79548df81
Kirill Dolgiy <maindolgiykirill99@gmail.com>
2022-01-04 15:03:15 +0300

    AddFirst and AddLast methods implemented instead of Add method. PrintAllList implemented.

--- Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
+++ Alogithms and Data Structures/LinkedList/LinkedList/LinkedList.cpp
@@ -16,36 +16,66 @@ class List
 {
 public:
     Node* First;
+    Node* Last;
 
     List(Node* node) {
         this->First = node;
+        this->Last = node;
     }
 
     List() {
         this->First = nullptr;
+        this->Last = nullptr;
     }
 
     bool IsEmpty() {
         return this->First == NULL;
     }
 
-    void Add(Node* node) {
-        if (this->IsEmpty() != true) {
+    void AddFirst(Node* node) {
+        if (this->IsEmpty()) {
+            this->First = node;
+            this->Last = node;
+        }
+        else {
             Node* tmp = this->First;
 
             this->First = node;
             this->First->Next = tmp;
-
-            free(tmp);
-        }
-        else {
-            this->First = node;
         }
 
     }
+
+    void AddLast(Node* node) {
+        Node* tmp = this->Last;
+
+        this->Last->Next = node;
+        this->Last = node;
+    }
+
+    void PrintAllList(Node* startNode) {
+        if (startNode != nullptr) {
+            std::cout << startNode->Data << "\n";
+            if (startNode->Next != nullptr) {
+                PrintAllList(startNode->Next);
+            }
+        }
+    }
+
 };
 
 int main()
 {
+    List l = List();
+
+    Node n1(nullptr, 10);
+    Node n2(nullptr, 13);
+    Node n3(nullptr, 17);
+
+    l.AddFirst(&n1);
+    l.AddFirst(&n2);
+    l.AddLast(&n3);
+
+    l.PrintAllList(l.First);
 
 }
