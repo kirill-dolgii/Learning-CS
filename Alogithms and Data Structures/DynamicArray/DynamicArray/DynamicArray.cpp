e58d2f542f535b152fffb1561e02175fe7f46149
Kirill Dolgiy <maindolgiykirill99@gmail.com>
2022-01-06 21:36:40 +0300

    DynamicArray class with Size and IsEmpty methods is implemented.

---
+++ Alogithms and Data Structures/DynamicArray/DynamicArray/DynamicArray.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class DynamicArray {
+
+public:
+	DynamicArray(int length) {
+		capacity =  length;
+		len = length;
+
+		vector<T> vec(length);
+		data = vec;
+	};
+
+	int Size() { return len; };
+	bool IsEmpty() { return this->Size() == 0; };
+
+	T* Get(int index) { return &data[index]; };
+	void Set(int index, T value) { data[index] = value; };
+
+	~DynamicArray();
+
+private:
+	vector<T> data;
+	int len;
+	int capacity;
+};
+
+
+int main() {
+	//DynamicArray<int>* asd = new DynamicArray<int>(3);
+
+	//asd->Set(0, 3);
+	//asd->Set(1, 2);
+	//asd->Set(2, 1);
+
+	//int* c = asd->Get(0);
+	//*c = 5;
+	//cout << 3;
+} 
