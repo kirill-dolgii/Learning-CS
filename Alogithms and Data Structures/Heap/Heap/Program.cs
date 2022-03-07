9df94ad17eb77fa6ee2dc26016b4d9ecf5b1e2c4
Kirill Dolgiy <maindolgiykirill99@gmail.com>
2022-03-07 14:32:22 +0300

    O(n) constructor is implemented. ExtractMin is implemented.

--- Alogithms and Data Structures/Heap/Heap/Program.cs
+++ Alogithms and Data Structures/Heap/Heap/Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
+
 namespace Heap
-{
-	
+
+{	// Author : Dolghi Chirill, 07.03.2022
+	//-------------------------------------------------------------------------------------------
+	// this data structure implements priority queue -> heap (data is stored inside of an array).
+	// both min and max queues are supported.
+	// priority queue operations: insert O(log(n) and extractHead O(log(N)) are supported.
+	// both O(n) and O(log(N)) constructors are implemented.
+
 	internal class MyHeap<T>
 		where T : IComparable<T>
 	{
@@ -15,18 +23,28 @@ namespace Heap
 
 		private const uint DEFAULT_CAPACITY = 10;
 
+		private IComparer<T> comparer;
+
+		bool IsMinHeap = true;
+
 		public MyHeap() : this(DEFAULT_CAPACITY) { }
 
-		public MyHeap(uint capacity)
+		public MyHeap(uint capacity, bool IsMinHeap = false)
 		{
+			this.IsMinHeap = IsMinHeap;
+
 			this.capacity = capacity;
 			container = new T[capacity];
 		}
 
-		public MyHeap(T[] data)
+		// O(n * logN) heap constructor
+		public MyHeap(T[] data, bool IsMinHeap)
 		{
+			this.IsMinHeap = IsMinHeap;
+
 			container = new T[data.Count()];
 			capacity = (uint)data.Count();
+			size = (uint)data.Count();
 
 			foreach (T item in data)
 			{
@@ -34,9 +52,27 @@ namespace Heap
             }
         }
 
+		// O(n) heap constructor (bubble down all nodes from the half untill the beginning)
+		public MyHeap(ICollection<T> data, bool IsMinHeap = false)
+		{
+			this.IsMinHeap = IsMinHeap;	
+
+			container = new T[data.Count()];
+			data.CopyTo(container, 0);
+			capacity = (uint)data.Count();
+			size = (uint)data.Count();
+
+			for (int i = ((int)size / 2) - 1; i >= 0; i--)
+				this.BubbleDown((uint)i);
+        }
+
 		public bool IsEmpty() => size == 0;
 
-		public void Clear() { this.container = new T[capacity < DEFAULT_CAPACITY ? DEFAULT_CAPACITY : capacity];}
+		public void Clear() 
+		{ 
+			this.container = new T[capacity < DEFAULT_CAPACITY ? DEFAULT_CAPACITY : capacity];
+			size = 0;
+		}
 
 		public uint Size() => size;
 
@@ -44,6 +80,7 @@ namespace Heap
 
 		public bool Contains(T elem) => container.Contains(elem);
 
+		// O(log(N)) 
 		public void Insert(T item)
 		{
 			if (size >= capacity)
@@ -75,26 +112,37 @@ namespace Heap
 			return true;
 		}
 
+		public T ExtractHead()
+		{
+			T ret = container[0];
+			this.Remove(ret);
+			size--;
+			return ret;
+        }
+
 		private void BubbleDown(uint index)
 		{
-			if (2 * index + 1 > size)
+			if (2 * index + 1 >= size)
 				return;
 
 			uint minIdx = index;
 
-			for (int i = 1; i <= 2; i++)
+			for (int i = 1; i <= 2 && index * 2 + i < size - 1; i++)
 				{
-				if (Compare(container[index], container[index * 2 + i]))
+
+				if (Compare(container[minIdx], container[index * 2 + i]))
 					minIdx = (uint)(index * 2 + i);
 				}
-
-			Swap(index, minIdx);
-			BubbleDown(minIdx);
+			if (minIdx != index)
+			{
+				Swap(index, minIdx);
+				BubbleDown(minIdx);
+			}			
 		}
 
 		private void BubbleUp(uint index)
 		{
-			if (index == 0)
+			if (index == 0 || index >= size)
 				return;
 
 			uint parent = (index - 1) / 2;
@@ -108,15 +156,24 @@ namespace Heap
 
 		private void Swap(uint item1, uint item2)
 		{
+			if (item1 == item2)
+				return;
+
 			var tmp   = container[item1];
 			container[item1] = container[item2];
 			container[item2] = tmp;
 		}
 
-		private bool Compare(T item1, T item2) => item1.CompareTo(item2) < 0 ? true : false;
+		private bool Compare(T item1, T item2) 
+		{
+			if (IsMinHeap)
+				return item1.CompareTo(item2) > 0 ? true : false;
+			else
+				return item1.CompareTo(item2) < 0 ? true : false; 
+		}
 	}
 
-	internal class Program
+    internal class Program
 	{
 		private static void Main(string[] args)
 		{
@@ -130,9 +187,16 @@ namespace Heap
 			mh.Insert(24);
 			mh.Insert(1);
 
+
 			int[] d = new int[] { 0, 2, 3, 7, 19, 24, 1 };
 
-			var mh1 = new MyHeap<int>(d);
+			var mh1 = new MyHeap<int>(new List<int>(d), true);
+
+			mh1.ExtractHead();
+			mh1.ExtractHead();
+			mh1.ExtractHead();
+			mh1.ExtractHead();
+			mh1.ExtractHead();
 
 		}
 	}
