daf0e0fe1c225e31a88debba0f6e02bd6415e520
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-15 18:26:04 +0300

    Created a new Algorithms project, implemented HeapSort method

---
+++ Alogithms and Data Structures/Algorithms/Sorting.cs
@@ -0,0 +1,22 @@
+﻿using DataStructures;
+
+
+namespace Algorithms;
+public static class Sorting
+
+{
+	/// <summary>
+	///     Sorts input data using heap sort algorithm.
+	/// </summary>
+	/// <param name="data">Input data.</param>
+	/// <param name="ascending">Specify sorting order.</param>
+	public static IEnumerable<T> HeapSort<T>(IEnumerable<T> data, bool ascending = false)
+	where T : IComparable<T>
+	{
+		var          heap   = new Heap<T>(data, ascending);
+		foreach (T val in data) yield return heap.ExtractHead();
+	}
+
+
+
+}
