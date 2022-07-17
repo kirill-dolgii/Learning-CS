8bd72a57f6d01eccf8e30ed6bad4980608286d20
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-17 21:09:39 +0300

    Refactoring of sorting tests. TestDataGenerator<T> is implemented. TestHelper<T> class is implemented in order to simplify testing of SortingAlgorithms with several data structures.

---
+++ Alogithms and Data Structures/AlgorithmsTests/SortersTests/SorterTestHelper.cs
@@ -0,0 +1,103 @@
+﻿using Algorithms.Sorting;
+
+namespace AlgorithmsTests.SortersTests;
+
+[TestClass]
+public class SorterTestHelper<T>
+where T : IComparable<T>
+{
+	public SorterTestHelper(Func<T> generator, ISorter<T> sorter, IComparer<T> comparer, TestContext tc)
+    {
+		this.Generator = new TestDataGenerator<T>(generator);
+        this.Sorter = sorter;
+        this.Comparer = comparer;
+        this.tc = tc;
+    }
+
+    private TestContext tc { get; }
+
+	private TestDataGenerator<T> Generator { get; }
+
+	private void PrepareTestData()
+	{
+		var testData = Generator.GenerateData();
+		this.Input = testData.input;
+		this.ExpectedOutput = testData.expdOutput;
+	}
+
+	[TestMethod]
+	public void Sort_Ascending()
+	{
+		PrepareTestData();
+		Sorter.Sort(this.Input);
+		var testEnumerator   = this.Input.GetEnumerator();
+		var sortedEnumerator = this.ExpectedOutput.GetEnumerator();
+
+		for (int i = 0; i < this.Input.Length; i++)
+		{
+			testEnumerator.MoveNext();
+			sortedEnumerator.MoveNext();
+			Assert.AreEqual(sortedEnumerator.Current, testEnumerator.Current);
+		}
+	}
+
+	[TestMethod]
+	public void Sort_Descending()
+	{
+		PrepareTestData();
+		Sorter.Sort(this.Input, SortingOrder.Descending);
+		var testEnumerator   = this.Input.GetEnumerator();
+		var sortedEnumerator = this.ExpectedOutput.Reverse().GetEnumerator();
+
+		for (int i = 0; i < this.Input.Length; i++)
+		{
+			testEnumerator.MoveNext();
+			sortedEnumerator.MoveNext();
+			Assert.AreEqual(sortedEnumerator.Current, testEnumerator.Current);
+		}
+
+		sortedEnumerator.Dispose();
+	}
+
+	private IComparer<T> Comparer { get; }
+
+	[TestMethod]
+	public void Sort_Ascending_Comparer()
+	{
+		PrepareTestData();
+		Sorter.Sort(this.Input, Comparer);
+		var testEnumerator   = this.Input.GetEnumerator();
+		var sortedEnumerator = this.ExpectedOutput.GetEnumerator();
+
+		for (int i = 0; i < this.Input.Length; i++)
+		{
+			testEnumerator.MoveNext();
+			sortedEnumerator.MoveNext();
+			Assert.AreEqual(sortedEnumerator.Current, testEnumerator.Current);
+		}
+	}
+
+	[TestMethod]
+	public void Sort_Descending_Comparer()
+	{
+		PrepareTestData();
+		Sorter.Sort(this.Input, Comparer, SortingOrder.Descending);
+		var testEnumerator   = this.Input.GetEnumerator();
+		var sortedEnumerator = this.ExpectedOutput.Reverse().GetEnumerator();
+
+		for (int i = 0; i < this.Input.Length; i++)
+		{
+			testEnumerator.MoveNext();
+			sortedEnumerator.MoveNext();
+			Assert.AreEqual(sortedEnumerator.Current, testEnumerator.Current);
+		}
+
+		sortedEnumerator.Dispose();
+	}
+
+	private ISorter<T> Sorter { get; }
+
+	private T[] Input { get; set; } = null!;
+
+	private T[] ExpectedOutput { get; set; } = null!;
+}
---
+++ Alogithms and Data Structures/AlgorithmsTests/SortersTests/SorterTests.cs
@@ -0,0 +1,61 @@
+﻿using Algorithms.Sorting;
+
+namespace AlgorithmsTests.SortersTests;
+
+[TestClass]
+public class SorterTests
+{
+    private class TestComparer<T> : IComparer<T>
+    {
+        public TestComparer(Func<T, T, int> compFunc) { this.CompFunc = compFunc; }
+
+        private Func<T, T, int> CompFunc { get; }
+
+        public int Compare(T? x, T? y)
+        {
+            return CompFunc.Invoke(x, y);
+        }
+    }
+
+	private TestContext _testContextInstance = null!;
+	public TestContext TestContext
+	{
+		get => _testContextInstance;
+		set => _testContextInstance = value;
+	}
+
+    [TestMethod]
+    public void HEAP_SORT_INT()
+    {
+        int IntCompFunc(int x, int y) => x.CompareTo(y);
+        var comp = new TestComparer<int>(IntCompFunc);
+
+        Random random = new Random();
+
+        var heapSorter = new HeapSorter<int>();
+
+        var helper = new SorterTestHelper<int>(() => random.Next(10000), heapSorter, comp, _testContextInstance);
+        helper.Sort_Ascending();
+		helper.Sort_Descending();
+        helper.Sort_Ascending_Comparer();
+        helper.Sort_Descending_Comparer();
+    }
+
+	[TestMethod]
+	public void HEAP_SORT_DBL()
+	{
+		int IntCompFunc(double x, double y) => x.CompareTo(y);
+		var comp = new TestComparer<double>(IntCompFunc);
+
+		Random random = new Random();
+
+		var heapSorter = new HeapSorter<double>();
+
+		var helper = new SorterTestHelper<double>(() => random.Next(10000), heapSorter, comp, _testContextInstance);
+		helper.Sort_Ascending();
+		helper.Sort_Descending();
+		helper.Sort_Ascending_Comparer();
+		helper.Sort_Descending_Comparer();
+	}
+}
+
