3320c3fbf1c5604e3b581170ea0f8e06fc89bb4a
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-17 15:44:28 +0300

    Refactoring of sorting algorithms

---
+++ Alogithms and Data Structures/Algorithms/Sorting/ISorter.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.Sorting;
+
+internal interface ISorter<T>
+where T : IComparable<T>
+{
+	/// <summary>
+	/// Sorts sequence by applying default comparer of generic type
+	/// </summary>
+	/// <param name="data"></param>
+	/// <param name="order"></param>
+	public static void Sort(T[] data, SortingOrder order = SortingOrder.Ascending) {}
+
+	/// <summary>
+	/// Sort sequence with specific comparer
+	/// </summary>
+	/// <param name="data"></param>
+	/// <param name="comparer"></param>
+	/// <param name="order"></param>
+	public static void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending) {}
+}
