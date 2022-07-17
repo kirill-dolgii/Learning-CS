4c05a5dee65e04652b96fa638813bdffd39725a2
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-17 15:45:57 +0300

    Refactoring of HeapSorter and HeapSorterTests

---
+++ Alogithms and Data Structures/Algorithms/Sorting/HeapSorter.cs
@@ -0,0 +1,30 @@
+﻿using DataStructures;
+
+namespace Algorithms.Sorting;
+
+public class HeapSorter<T> : ISorter<T>
+where T : IComparable<T>
+{
+	/// <summary>
+	///     Sorts input data using the heap sort algorithm.
+	/// </summary>
+	/// <param name="data">Input data.</param>
+	/// <param name="order">Sorting order.</param>
+	public static void Sort(T[] data, SortingOrder order = SortingOrder.Ascending)
+    {
+		var heap = new Heap<T>(data, order == SortingOrder.Ascending);
+		for (int i = 0; i < data.Length; i++) data[i] = heap.ExtractHead();
+	}
+
+	/// <summary>
+	///		Sorts input data using the heap sort algorithm and specified comparer.
+	/// </summary>
+	/// <param name="data">Input data.</param>
+	/// <param name="comparer">Custom comparer</param>
+	/// <param name="order">Sorting order.</param>
+	public static void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending)
+    {
+		var heap = new Heap<T>(data, comparer, order == SortingOrder.Ascending);
+		for (int i = 0; i < data.Length; i++) data[i] = heap.ExtractHead();
+	}
+}
