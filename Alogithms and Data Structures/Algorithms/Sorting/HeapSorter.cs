a2846ccd7d2db682ee6f1a20d2d95cc2e4052f99
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-17 21:11:50 +0300

    Refactoring of ISorter<T>

--- Alogithms and Data Structures/Algorithms/Sorting/HeapSorter.cs
+++ Alogithms and Data Structures/Algorithms/Sorting/HeapSorter.cs
@@ -5,12 +5,14 @@ namespace Algorithms.Sorting;
 public class HeapSorter<T> : ISorter<T>
 where T : IComparable<T>
 {
+	public HeapSorter() { }
+
 	/// <summary>
 	///     Sorts input data using the heap sort algorithm.
 	/// </summary>
 	/// <param name="data">Input data.</param>
 	/// <param name="order">Sorting order.</param>
-	public static void Sort(T[] data, SortingOrder order = SortingOrder.Ascending)
+	public void Sort(T[] data, SortingOrder order = SortingOrder.Ascending)
     {
 		var heap = new Heap<T>(data, order == SortingOrder.Ascending);
 		for (int i = 0; i < data.Length; i++) data[i] = heap.ExtractHead();
@@ -22,7 +24,7 @@ where T : IComparable<T>
 	/// <param name="data">Input data.</param>
 	/// <param name="comparer">Custom comparer</param>
 	/// <param name="order">Sorting order.</param>
-	public static void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending)
+	public void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending)
     {
 		var heap = new Heap<T>(data, comparer, order == SortingOrder.Ascending);
 		for (int i = 0; i < data.Length; i++) data[i] = heap.ExtractHead();
--- Alogithms and Data Structures/Algorithms/Sorting/ISorter.cs
+++ Alogithms and Data Structures/Algorithms/Sorting/ISorter.cs
@@ -1,6 +1,6 @@
 ﻿namespace Algorithms.Sorting;
 
-internal interface ISorter<T>
+public interface ISorter<T>
 where T : IComparable<T>
 {
 	/// <summary>
@@ -8,7 +8,7 @@ where T : IComparable<T>
 	/// </summary>
 	/// <param name="data"></param>
 	/// <param name="order"></param>
-	public static void Sort(T[] data, SortingOrder order = SortingOrder.Ascending) {}
+	public void Sort(T[] data, SortingOrder order = SortingOrder.Ascending);
 
 	/// <summary>
 	/// Sort sequence with specific comparer
@@ -16,5 +16,5 @@ where T : IComparable<T>
 	/// <param name="data"></param>
 	/// <param name="comparer"></param>
 	/// <param name="order"></param>
-	public static void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending) {}
+	public void Sort(T[] data, IComparer<T> comparer, SortingOrder order = SortingOrder.Ascending);
 }
