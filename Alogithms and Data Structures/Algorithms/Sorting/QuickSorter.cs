dc2c15d42205c926a6fc51b38c45c15ee2b1fca2
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-20 11:28:09 +0300

    QuickSorter<T> implementation started.

---
+++ Alogithms and Data Structures/Algorithms/Sorting/QuickSorter.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithms.Sorting;
+public class QuickSorter<T> : Sorter<T>
+where T : IComparable<T>
+{
+	public override void Sort(T[] data, SortingOrder order = SortingOrder.Ascending) { Sort(data, null, order); }
+
+	public override void Sort(T[] data, IComparer<T>? comparer, SortingOrder order = SortingOrder.Ascending)
+	{
+		
+	}
+
+	private void SubSort(T[] subData)
+	{
+
+	}
+
+	private void Partition(T[] data)
+	{
+		T pivot = data[data.Length / 2];
+
+		(data[data.Length / 2], data[^1]) = (data[^1], data[data.Length / 2]);
+
+	}
+}
+
