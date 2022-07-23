3939e82a9761ff43fdf1ba8d4c67ed38c40a5483
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-23 14:23:23 +0300

    QuickSorter<T> is implemented.

--- Alogithms and Data Structures/Algorithms/Sorting/QuickSorter.cs
+++ Alogithms and Data Structures/Algorithms/Sorting/QuickSorter.cs
@@ -2,24 +2,43 @@
 public class QuickSorter<T> : Sorter<T>
 where T : IComparable<T>
 {
-	public override void Sort(T[] data, SortingOrder order = SortingOrder.Ascending) { Sort(data, null, order); }
+	public override void Sort(T[] data, SortingOrder order = SortingOrder.Ascending)
+		=> this.Sort(data, null, order);
 
 	public override void Sort(T[] data, IComparer<T>? comparer, SortingOrder order = SortingOrder.Ascending)
 	{
-		
+		int split = Partition(data, comparer, order, 0, data.GetUpperBound(0));
+		Sort(data, comparer, order, 0, split - 1);
+		Sort(data, comparer, order, split + 1, data.GetUpperBound(0));
 	}
 
-	private void SubSort(T[] subData)
+	private int Partition(T[] data, IComparer<T>? comparer, SortingOrder order, int start, int end)
 	{
+		if (end - start > 2)
+		{
+			int pivot = new int[] { start, start + (end - start) / 2, end }.ToDictionary(idx => idx, idx => data[idx]).
+																			OrderBy(kv => kv.Value).
+																			ToList()[1].Key;
+			(data[end], data[pivot]) = (data[pivot], data[end]);
+		}
 
+		int i = start - 1;
+		for (int j = start; j <= end - 1; j++)
+			if (Compare(data[j], data[end], comparer) * ((int)order) <= 0)
+				(data[++i], data[j]) = (data[j], data[i]);
+
+		(data[end], data[++i]) = (data[i], data[end]);
+
+		return i;
 	}
 
-	private void Partition(T[] data)
+    private void Sort(T[] data, IComparer<T>? comparer, SortingOrder order, int start, int end)
 	{
-		T pivot = data[data.Length / 2];
+		if (start >= end || end < 0) return;
 
-		(data[data.Length / 2], data[^1]) = (data[^1], data[data.Length / 2]);
-
+		int split = Partition(data, comparer, order, start, end);
+		Sort(data, comparer, order, start, split - 1);
+		Sort(data, comparer, order, split + 1, end);
 	}
+
 }
-
--- Alogithms and Data Structures/AlgorithmsTests/SortersTests/SorterTests.cs
+++ Alogithms and Data Structures/AlgorithmsTests/SortersTests/SorterTests.cs
@@ -214,5 +214,22 @@ public class SorterTests
 		helper.Sort_Descending();
 	}
 
+	[TestMethod]
+	public void QUICK_SORT_INT()
+	{
+		int IntCompFunc(int x, int y) => x.CompareTo(y);
+		var comp = new TestComparer<int>(IntCompFunc);
+
+		Random random = new Random();
+
+		var mergeSorter = new QuickSorter<int>();
+
+		var helper = new SorterTestHelper<int>(() => random.Next(10000), mergeSorter, comp, _testContextInstance);
+		helper.Sort_Ascending();
+		helper.Sort_Descending();
+		helper.Sort_Ascending_Comparer();
+		helper.Sort_Descending_Comparer();
+	}
+
 }
 
