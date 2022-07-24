8b2e71cf7c7b665a72ee67b346200bef31e0ba75
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-24 14:09:26 +0300

    HashFunction<T> class is implemented.

---
+++ Alogithms and Data Structures/DataStructures/HashTable/HashFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructures.HashTable;
+
+public class HashFunction<T>
+{
+	private readonly Func<T, int>? _hashGenerator;
+
+	public HashFunction() {}
+
+	protected HashFunction(Func<T, int> hashGenerator) => _hashGenerator = hashGenerator;
+
+	public int GetHash(T value)
+	{
+		if (value == null) throw new NullReferenceException(nameof(value));
+		return _hashGenerator == null ? value.GetHashCode() : _hashGenerator!.Invoke(value);
+	}
+}
+
