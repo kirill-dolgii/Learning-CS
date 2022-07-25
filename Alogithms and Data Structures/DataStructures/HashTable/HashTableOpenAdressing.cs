418055b4b32ad58b2b331d895c53c2036a037832
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-25 23:00:56 +0300

    HashTableOpenAddressing<TKey, TValue> Remove and Contains are implemented.

--- Alogithms and Data Structures/DataStructures/HashTable/HashTableOpenAdressing.cs
+++ Alogithms and Data Structures/DataStructures/HashTable/HashTableOpenAdressing.cs
@@ -3,15 +3,17 @@
 namespace DataStructures.HashTable;
 public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TKey, TValue>
 {
-	private struct KeyValuePairEntity
+	private class KeyValuePairEntity
 	{
 		public bool                       isDeleted;
 		public KeyValuePair<TKey, TValue> kv;
-		public KeyValuePairEntity(KeyValuePair<TKey, TValue> kv) { this.kv = kv; isDeleted = false; }
+		public KeyValuePairEntity(KeyValuePair<TKey, TValue> kv, bool isDeleted) { this.kv = kv; this.isDeleted = isDeleted; }
+
+		public void Delete() { isDeleted = true; }
 	}
 
 
-	private const int    DEFAILT_CAPACITY    = 32;
+	private const int    DEFAILT_CAPACITY    = 4;
 	private const double DEFAULT_LOAD_FACTOR = 0.75;
 	private const double RESIZE_SCALE        = 0.65;
 
@@ -26,6 +28,8 @@ public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TK
 	private double             _loadFactor;
 	private HashFunction<TKey> _hf;
 
+	private List<KeyValuePairEntity> _addedValues;
+
 	private HashTableOpenAddressingBase(IEnumerable<KeyValuePair<TKey, TValue>> data, 
 								       int initialCapacity, 
 								       double loadFactor, HashFunction<TKey> hf)
@@ -36,6 +40,7 @@ public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TK
 
 		_capacity = new int[] { DEFAILT_CAPACITY, initialCapacity, data.Count() }.Max();
 		_entities = new KeyValuePairEntity?[_capacity];
+		_addedValues = new List<KeyValuePairEntity>(_capacity);
 		_loadFactor = loadFactor;
 		_hf = hf;
 	}
@@ -47,32 +52,35 @@ public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TK
 
 	private int AdjustedHash(TKey key) => Math.Abs(_hf.GetHash(key)) % _capacity;
 
-	protected virtual int Probe(int index, int i) => index + i;
+	protected virtual int Probe(int index, int i) => (index + i) % _capacity;
 
 	public void Add(KeyValuePair<TKey, TValue> item)
 	{
+		if (item.Key == null) throw new NotSupportedException($"{nameof(item.Key)} was null.");
 		Resize();
 
-		if (item.Key == null) throw new NotSupportedException($"{nameof(item.Key)} was null.");
 		int index = AdjustedHash(item.Key);
 
-		for (var i = 1; _entities[index].HasValue; i++) index = Probe(index, i);
-		_entities[index] = new KeyValuePairEntity(item);
+		for (var i = 1; _entities[index] != null && !_entities[index]!.isDeleted; i++) index = Probe(index, i);
+		_entities[index] = new KeyValuePairEntity(item, false);
+		_addedValues.Add(_entities[index]!);
 		_size++;
 	}
 
 	private void Resize()
 	{
-		if (_size <= _capacity * _loadFactor || _size > _capacity * _loadFactor * RESIZE_SCALE) return;
+		if (_size < _capacity * _loadFactor) return;
 
-		var existing = _entities.Where(ent => ent is { isDeleted: false }).ToArray();
+		var existing = _addedValues.Where(ent => ent is { isDeleted: false }).ToArray();
 
 		if (_capacity >= _capacity * _loadFactor) _capacity = (int)(_capacity / _loadFactor / RESIZE_SCALE);
 		if (_capacity <= _capacity * _loadFactor * RESIZE_SCALE) _capacity = (int)(_capacity * _loadFactor);
-
+		
 		_entities = new KeyValuePairEntity?[_capacity];
+		_addedValues = new List<KeyValuePairEntity>(_capacity);
+		_size = 0;
 
-		foreach (var ex in existing) this.Add(ex.Value.kv);
+		foreach (var ex in existing) this.Add(ex.kv);
 	}
 
 	public void Clear()
@@ -82,7 +90,18 @@ public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TK
 
 	public bool Contains(KeyValuePair<TKey, TValue> item)
 	{
-		throw new NotImplementedException();
+		if (item.Key == null) throw new NotSupportedException($"{nameof(item.Key)} was null.");
+		int index = AdjustedHash(item.Key);
+
+		if (_entities[index] == null) return false;
+
+		for (int i = 1; _entities[index] != null; i++)
+		{
+			if (EqualityComparer<TValue>.Default.Equals(_entities[index]!.kv.Value, item.Value)) 
+				return true;
+			index = Probe(index, i);
+		}
+		return false;
 	}
 
 	public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
@@ -90,9 +109,26 @@ public abstract class HashTableOpenAddressingBase<TKey, TValue> : IDictionary<TK
 		throw new NotImplementedException();
 	}
 
-	public bool Remove(KeyValuePair<TKey, TValue> item)
+    public bool Remove(KeyValuePair<TKey, TValue> item)
 	{
-		throw new NotImplementedException();
+		if (item.Key == null) throw new NotSupportedException($"{nameof(item.Key)} was null.");
+		int index = AdjustedHash(item.Key);
+
+		if (_entities[index] == null) return false;
+		for (int i = 1; _entities[index] != null; i++)
+		{
+			if (EqualityComparer<TValue>.Default.Equals(_entities[index]!.kv.Value, item.Value))
+			{
+				_entities[index]!.Delete();
+				_entities[index] = new KeyValuePairEntity(item, true);
+				_size--;
+				return true;
+			}
+
+			index = Probe(index, i);
+		}
+
+		return false;
 	}
 
 	public int  Count      { get; }
