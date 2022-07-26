35f39eb53d961ee870336658d7141d0280c298f8
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-26 22:52:07 +0300

    HashTableLinearProbing<TKey, TValue> class and tests are added.

---
+++ Alogithms and Data Structures/DataStructures/HashTable/HashTableLinearProbing.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructures.HashTable;
+public class HashTableLinearProbing<TKey, TValue> : HashTableOpenAddressingBase<TKey, TValue>
+{
+	public HashTableLinearProbing() : base() {}
+
+	public HashTableLinearProbing(IEnumerable<KeyValuePair<TKey, TValue>> data) : base(data) {}
+
+	public HashTableLinearProbing(IEnumerable<KeyValuePair<TKey, TValue>> data, 
+								  HashFunction<TKey> hf) : base(data, hf) {}
+
+	protected override int Probe(int index, int i) => (index + i) % this._capacity;
+}
+
---
+++ Alogithms and Data Structures/DataStructuresTests/HashTableOpenAddressingTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+using DataStructures.HashTable;
+
+namespace DataStructuresTests;
+
+[TestClass]
+public class HashTableOpenAddressingTests
+{
+	private record Person(string Name, int Age);
+
+	private record City(string Name, int Population);
+
+	private IDictionary<Person, City>                 _expectedDict;
+	private HashTableOpenAddressingBase<Person, City> _testDict;
+	private IEnumerable<KeyValuePair<Person, City>>   _testData;
+	private HashFunction<Person>                      _hf;
+
+	private string RandomString(int length, Random random)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+									.Select(s => s[random.Next(s.Length)]).ToArray());
+	}
+
+	[TestInitialize]
+	public void Initialize()
+	{
+		var rand = new Random(323);
+		int size = 100;
+
+		_testData = Enumerable.Range(0, size).Select(i => new KeyValuePair<Person, City>(
+															  new Person(RandomString(rand.Next(10), rand), rand.Next()),
+															  new City(RandomString(rand.Next(10), rand), rand.Next()))).
+							   ToArray();
+        _testDict = new HashTableLinearProbing<Person, City>(_testData);
+        _expectedDict = new Dictionary<Person, City>(_testData);
+		_hf = new HashFunction<Person>(p => p.Age * p.Name.GetHashCode());
+	}
+
+	[TestMethod]
+	public void CONSTRUCTOR_FROM_ENUMERABLE()
+	{
+		_testDict = new HashTableLinearProbing<Person, City>(_testData, _hf);
+		foreach (var kv in _testData) Assert.IsTrue(_testDict.Contains(kv));
+	}
+
+}	
+	
