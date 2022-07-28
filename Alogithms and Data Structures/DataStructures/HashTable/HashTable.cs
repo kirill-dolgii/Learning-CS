e3bf35329b0e8c58f98d131bf17c923b49f76620
Kirill Dolghi <maindolgiykirill99@gmail.com>
2022-07-28 19:13:13 +0300

    HashTable<TKey, TValye>.KeyValuePairEntity is implemented.

--- Alogithms and Data Structures/DataStructures/HashTable/HashTable.cs
+++ Alogithms and Data Structures/DataStructures/HashTable/HashTable.cs
@@ -5,15 +5,24 @@ namespace DataStructures.HashTable;
 
 public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 {
+	private class KeyValuePairEntity
+	{
+		public bool                       IsDeleted;
+		public KeyValuePair<TKey, TValue> Kv;
+		public KeyValuePairEntity(KeyValuePair<TKey, TValue> kv, bool isDeleted) { this.Kv = kv; this.IsDeleted = isDeleted; }
+
+		public void Delete() { IsDeleted = true; }
+	}
+
 	private const int    DEFAULT_CAPACITY    = 32;
 	private const double DEFAULT_LOAD_FACTOR = 0.75;
 	private const double RESIZE_SCALE        = 0.65;
 
 	private readonly IEqualityComparer<TKey> _keyComp = EqualityComparer<TKey>.Default;
 
-	private LinkedList<KeyValuePair<TKey, TValue>>[] _buckets;
-	private int                  _size;
-	private int                  _capacity;
+	private LinkedList<KeyValuePairEntity>[] _buckets;
+	private int                                            _size;
+	private int                                            _capacity;
 
 	private readonly double              _loadFactor;
 	private readonly HashFunction<TKey> _hashFunc;
@@ -27,7 +36,7 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 		if (initialCapacity < 0) throw new ArgumentException(nameof(initialCapacity));
 		if (loadFactor < 0) throw new ArgumentException(nameof(loadFactor));
 
-		_buckets = new LinkedList<KeyValuePair<TKey, TValue>>[initialCapacity];
+		_buckets = new LinkedList<KeyValuePairEntity>[initialCapacity];
 		_size = kvs.Count();
         _capacity = new int[] { initialCapacity, DEFAULT_CAPACITY, kvs.Count()}.Max();
         _loadFactor = loadFactor;
@@ -56,12 +65,10 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 			throw new NotSupportedException($"{nameof(item.Key)} key is already presented in the hash table.");
 
 		Resize();
-		
-		int hash = _hashFunc.GetHash(item.Key);
-		int bucketIndex = AdjustIndex(hash);
+		int index = AdjustedHash(item.Key);
 
-		_buckets[bucketIndex] ??= new();
-		_buckets[bucketIndex].AddLast(item);
+		_buckets[index] ??= new();
+		_buckets[index].AddLast(new KeyValuePairEntity(item, false));
 
 		_size++;
 	}
@@ -70,12 +77,12 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 	{
 		if (_size <= _capacity * _loadFactor && _size <= _capacity * Math.Pow(_loadFactor, 2)) return;
 
-		var tmp = _buckets.Where(b => b != null).SelectMany(b => b).ToArray();
+		var tmp = _buckets.Where(b => b != null).SelectMany(b => b.Select(ent => ent.Kv)).ToArray();
 
 		if (_capacity >= _capacity * _loadFactor) _capacity = (int)(_capacity / _loadFactor / RESIZE_SCALE);
 		if (_capacity <= _capacity * _loadFactor * RESIZE_SCALE) _capacity = (int)(_capacity * _loadFactor);
 
-		_buckets = new LinkedList<KeyValuePair<TKey, TValue>>[_capacity];
+		_buckets = new LinkedList<KeyValuePairEntity>[_capacity];
 		_size = 0;
 
 		foreach (var kv in tmp) this.Add(kv);
@@ -84,7 +91,7 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 	public void Clear()
 	{
 		_size = 0;
-		_buckets = new LinkedList<KeyValuePair<TKey, TValue>>[_capacity];
+		_buckets = new LinkedList<KeyValuePairEntity>[_capacity];
 		Resize();
 	}
 
@@ -94,7 +101,7 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 		int index = AdjustedHash(key);
 		if (_buckets[index] == null || _buckets[index].First == null) return -1;
 		var head = _buckets[index].First;
-		for (int i = 0; !_keyComp.Equals(head!.Value.Key, key); head = head.Next) 
+		for (int i = 0; !_keyComp.Equals(head!.Value.Kv.Key, key); head = head.Next) 
 			if (head.Next == null) return -1;
 		return index;
 	} 
@@ -134,13 +141,13 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 		return true;
 	}
 	
-	private LinkedListNode<KeyValuePair<TKey, TValue>>? BucketFindKey(LinkedList<KeyValuePair<TKey, TValue>> list, TKey key)
+	private LinkedListNode<KeyValuePairEntity>? BucketFindKey(LinkedList<KeyValuePairEntity> list, TKey key)
 	{
 		if (key == null) throw new ArgumentNullException(nameof(key));
 		if (list == null) throw new ArgumentNullException(nameof(key));
 
 		for (var node = list.First; node != null; node = node.Next) 
-			if (EqualityComparer<TKey>.Default.Equals(node.Value.Key, key)) return node;
+			if (EqualityComparer<TKey>.Default.Equals(node.Value.Kv.Key, key)) return node;
 		return null;
 	}
 
@@ -162,7 +169,7 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 
 			if (_buckets[adjustedHash] == null) throw new KeyNotFoundException(nameof(key));
 
-			var ret = _buckets[adjustedHash].FirstOrDefault(node => EqualityComparer<TKey>.Default.Equals(node.Key, key)).Value;
+			var ret = _buckets[adjustedHash].FirstOrDefault(node => EqualityComparer<TKey>.Default.Equals(node.Kv.Key, key)).Kv.Value;
 			if (EqualityComparer<TValue>.Default.Equals(ret, default(TValue))) throw new KeyNotFoundException(nameof(key));
 
 			return ret;
@@ -170,14 +177,14 @@ public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 		set => Add(new KeyValuePair<TKey, TValue>(key, value));
 	}
 	
-	public ICollection<TKey> Keys => _buckets.Where(b => b != null).SelectMany(b => b.ToArray()).Select(kv => kv.Key).ToList();
+	public ICollection<TKey> Keys => _buckets.Where(b => b != null).SelectMany(b => b.ToArray()).Select(kv => kv.Kv.Key).ToList();
 
-	public ICollection<TValue> Values => _buckets.Where(b => b != null).SelectMany(b => b.ToArray()).Select(kv => kv.Value).ToList();
+	public ICollection<TValue> Values => _buckets.Where(b => b != null).SelectMany(b => b.ToArray()).Select(kv => kv.Kv.Value).ToList();
 	
 	public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
 	{
 		var enumerable = _buckets.Where(b => b != null).SelectMany(b => b.ToArray());
-		return enumerable.GetEnumerator();
+		return enumerable.Select(ent => ent.Kv).GetEnumerator();
 	}
 
 	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
