4559d36eecd909b0ca78c64c0f7d0da8b684eb4c
peperonchinni <maindolgiykirill99@gmail.com>
2022-10-21 21:06:00 +0300

    Notes project implementation is started.

---
+++ WEB/Notes/Notes/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    public class Note
+    {
+		public Guid      User         { get; set; }
+		public Guid      Id           { get; set; }
+		public string    Title        { get; set; }
+		public string    Details      { get; set; }
+		public DateTime  CreationTime { get; set; }
+		public DateTime? DateTime     { get; set; }
+
+	}
+}
