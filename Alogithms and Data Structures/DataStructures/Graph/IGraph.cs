fc59de576e92d790f388e385d059b6f1e54d0df1
WawyWaker <maindolgiykirill99@gmail.com>
2023-02-20 22:24:34 +0300

    Graph class and tests are implemented.

--- Alogithms and Data Structures/DataStructures/Graph/IGraph.cs
+++ Alogithms and Data Structures/DataStructures/Graph/IGraph.cs
@@ -1,11 +1,19 @@
 ﻿namespace DataStructures.Graph;
 
-public interface IGraph<T>
+public interface IGraph<TNode, TEdge>
 {
-	public IEnumerable<T> Adjacent(T node);
-	public void AddNode(T node);
-	public void AddEdge(T x, T y);
-	public void RemoveEdge(T x, T y);
-	public int VerticeCount { get; }
-	public IEnumerable<T> Vertices { get; }
+	public ICollection<TNode> AdjacentNodes(TNode node);
+	public void AddNode(TNode node);
+	public bool ContainsNode(TNode node);
+	public bool RemoveNode(TNode node);
+	public int Degree(TNode node);
+	public void AddEdge(TNode x, TNode y, TEdge edge);
+	public bool ContainsEdge(TNode x, TNode y, TEdge edge);
+	public bool RemoveEdge(TNode x, TNode y, TEdge edge);
+	public ICollection<TNode> Vertices { get; }
+	public int VerticesCount { get; }
+	public ICollection<TEdge> Edges(TNode x, TNode y);
+    public ICollection<TEdge> Edges(TNode x);
+	public int EdgesCount { get; }
+	public bool Directed { get; }
 }
