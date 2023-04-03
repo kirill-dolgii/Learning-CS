0d9b464af60a2c1cf4c8e555379aefb28d7fab10
peperonchinni <maindolgiykirill99@gmail.com>
2023-04-03 19:10:00 +0300

    Fibonacci numbers generator is implemented

---
+++ Alogithms and Data Structures/Algorithms/Numeric/Sequences/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithms.Numeric.Sequences;
+
+public static class Fibonacci
+{
+	public static IEnumerable<int> FibonacciSequence()
+	{
+		var n1 = 0;
+		var n2 = 1;
+		yield return n1;
+		yield return n2;
+		while (true)
+		{
+			var next = n1 + n2;
+			n1 = n2;
+			n2 = next;
+			yield return next;
+		}
+	}
+}
